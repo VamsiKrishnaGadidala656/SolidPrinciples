ddf6b4f5bedcc241ae480744ba42a5bedc6100d3
VamsiKrishna656 <vamsi.gadidala@vassarlabs.com>
2024-09-17 00:04:11 +0530

    Added DIP

---
+++ src/main/java/DIP/ProblematicCode/Main.java
@@ -0,0 +1,12 @@
+package DIP.ProblematicCode;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+            DIP ::
+            -> Instead of depending on concrete classes, you should depend on a abstraction.
+            -> High level modules should not depend on low level modules.
+         */
+    }
+}
